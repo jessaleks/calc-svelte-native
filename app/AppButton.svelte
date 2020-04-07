c2edfb305582218779bf020de5a7e1efb73f5cf4
Aleksander Jess <jess.aleksander@gmail.com>
2020-04-07 21:07:32 +0200

    almost there

--- app/AppButton.svelte
+++ app/AppButton.svelte
@@ -1,4 +1,9 @@
-<button text={label} on:tap={()=>{$calculation += label}} row={row} col={col}>
+<!-- @param label   used to display text in the button 
+     @param row     used to place the component in the right row
+     @param col     used to place the component in the rightcolumn -->
+
+
+<button text={label} on:tap={()=>{handleInput(label)}} row={row} col={col}>
 
 </button>
 
@@ -7,20 +12,28 @@
 
 // @param text is the displayed text on each key
 //        it is passed down from AppButtonRow component 
+
+
+
 function handleInput(text) {
-    const specialKeys = ["AC", "C", "%", "/", "X", "+", "-", "="]
-    specialKeys.forEach(sk => {
-        if (text == sk) {
-            // special functionality like delete, etc
-        }
-        else {
-            $calculation += label
-        }
-    });
+    const specialKeys = ["AC", "C", "="]
+    if (text == "AC") {
+        console.log("AC");
+    }
+    else if (text == "C") {
+        console.log("C");
+    }
+    else if (text == "=") {
+        $result = eval($calculation);
+    }
+    else {
+        $calculation += label;
+    }
+};
 
-}
 
-import {calculation} from './store.js'
+
+import {calculation, result} from './store.js'
 //export let 
 export let label = "";
 export let row;
